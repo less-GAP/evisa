b37b04de75663bd9c275c81624d487f6c2935e8e
Phuong <phuong@lessgap.com>
2023-07-30 17:39:34 +0700

    build production

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 docker-compose -f docker-compose.develop.yml exec admin_dev npm run build
+rm -rf ./public/cpn1234
 mv  ./modules/Admin/public ./public/cpn1234
 docker-compose -f docker-compose.develop.yml exec frontend_dev npm run build
 cp -r ./modules/Frontend/public/* ./public/
