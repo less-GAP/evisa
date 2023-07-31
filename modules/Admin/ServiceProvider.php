5ae1c52e4248b4ec54191772d182529248a858c0
Phuong <phuong@lessgap.com>
2023-07-31 21:51:36 +0700

    build production

--- modules/Admin/ServiceProvider.php
+++ modules/Admin/ServiceProvider.php
@@ -6,7 +6,6 @@ namespace Modules\Admin;
 use App\Providers\AbstractModuleProvider;
 
 define('ADMIN_ROUTE_PREFIX', env('APP_ADMIN') . '/api');
-
 class ServiceProvider extends AbstractModuleProvider
 {
     protected $middleware = [];
