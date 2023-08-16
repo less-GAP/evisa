c033f94689d14401541cbd168a0c7d1d04797b23
cuongphan <minhcuong.phan@masterisehomes.com>
2023-08-16 19:29:18 +0700

    Update 16/08/2023

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (env('FORCE_HTTPS', false)) { // Default value should be false for local server
+            URL::forceScheme('https');
+        }
     }
 }
