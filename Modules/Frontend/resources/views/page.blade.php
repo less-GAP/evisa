2efa0892e4d93879057df95786e0e97a12cb0d8e
Phuong <phuong@lessgap.com>
2023-09-18 14:49:31 +0700

    hotfix apply page

--- Modules/Frontend/resources/views/page.blade.php
+++ Modules/Frontend/resources/views/page.blade.php
@@ -25,9 +25,7 @@ $content = \Blade::render($page->content);
 ?>
 
 <x-front::layout.sidebar>
-    <h1 class=" text-neutral-900 text-left font-semibold text-3xl md:text-4xl md:!leading-[120%] lg:text-5xl dark:text-neutral-100 max-w-4xl "
-        title="{{$page->title}}">{{$page->title}}</h1>
-    <span class="block text-base text-neutral-500 md:text-lg dark:text-neutral-400 pb-1">{{$page->excerpt}}</span>
+
     <div class="prose lg:prose-lg !max-w-screen-md mx-auto dark:prose-invert">
         {!! $content !!}
     </div>
