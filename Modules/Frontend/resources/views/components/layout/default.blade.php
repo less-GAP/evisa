626d33af96258536775f6b9bc2e9285c041a154f
Phuong <phuong@lessgap.com>
2023-09-18 14:44:27 +0700

    hotfix apply page

--- Modules/Frontend/resources/views/components/layout/default.blade.php
+++ Modules/Frontend/resources/views/components/layout/default.blade.php
@@ -1,16 +1,16 @@
+
 <x-front::layout.header>
 </x-front::layout.header>
 <x-splade-flash>
-    <p v-if="flash.has('message')" v-text="flash.message"/>
+    <p v-if="flash.has('message')" v-text="flash.message" />
 </x-splade-flash>
-<div id="primary" class="content-area">
-    <main id="main" class="site-main">
-        <div class="container">
-            {{ $slot }}
-        </div>
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
 
-    </main><!-- #main -->
-</div><!-- #primary -->
+            {{ $slot }}
+
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
 <x-front::layout.footer>
 </x-front::layout.footer>
