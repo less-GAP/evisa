5bd500c38b771a706c6898dccca566684c68375f
Phuong <phuong@lessgap.com>
2023-09-20 15:52:25 +0700

    Fix style

--- Modules/Frontend/resources/views/page.blade.php
+++ Modules/Frontend/resources/views/page.blade.php
@@ -27,9 +27,7 @@ $content = \Blade::render($page->content);
 <x-front::layout.sidebar>
     <h1 class="text-center mb-5 text-neutral-900 text-left font-semibold text-3xl md:text-4xl md:!leading-[120%] lg:text-5xl dark:text-neutral-100 max-w-4xl "
         title="{{$page->title}}">{{$page->title}}</h1>
-    <div class="prose lg:prose-lg !max-w-screen-md mx-auto dark:prose-invert">
-        {!! $content !!}
-    </div>
+    {!! $content !!}
 </x-front::layout.sidebar>
 
 
