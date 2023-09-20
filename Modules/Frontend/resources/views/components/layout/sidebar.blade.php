42c99eb00635790fcc12d6375bbc7db8fbf1eebe
Phuong <phuong@lessgap.com>
2023-09-20 11:39:19 +0700

    Fix style

--- Modules/Frontend/resources/views/components/layout/sidebar.blade.php
+++ Modules/Frontend/resources/views/components/layout/sidebar.blade.php
@@ -45,7 +45,7 @@
                     </li>
                 </ul>
                 <Link href="/apply"
-                      class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
+                      class="w-full bg-black text-white  text-lg font-semibold text-white uppercase  rounded flex items-center justify-center px-2 py-3 mt-4">
                     Apply now
                 </Link>
             </div>
