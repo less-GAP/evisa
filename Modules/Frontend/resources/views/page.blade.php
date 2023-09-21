e93f06a621d1cee930978fdf16a5de0c75bfd5ea
Phuong <phuong@lessgap.com>
2023-09-21 15:03:05 +0700

    fix seo

--- Modules/Frontend/resources/views/page.blade.php
+++ Modules/Frontend/resources/views/page.blade.php
@@ -9,27 +9,24 @@ if (!$page) {
     return;
 }
 
-SEO::title($page->title)
-    ->description($page->excerpt ?? '')
-    ->keywords($page->keywords ?? '');
-SEO::openGraphType('WebPage');
-SEO::openGraphSiteName(settings('site_title', ''));
-SEO::openGraphTitle($page->title);
-SEO::openGraphUrl(url($page->slug));
-if ($page->image) {
-    SEO::openGraphImage(url($page->image));
-}
+
 
 
 $content = \Blade::render($page->content);
 ?>
 
 <x-front::layout.sidebar>
+    @seoTitle($page->title)
+    @seoDescription($page->excerpt ?? '')
+    @seoKeywords($page->excerpt ?? '')
+    @if ($page->image)
+        @seoOpenGraphImage(url($page->image));
+    @endif
     <h1 class="text-center mb-5 text-neutral-900 text-left font-semibold text-3xl md:text-4xl md:!leading-[120%] lg:text-5xl dark:text-neutral-100 max-w-4xl "
         title="{{$page->title}}">{{$page->title}}</h1>
     <div id="page-content">
 
-    {!! $content !!}
+        {!! $content !!}
     </div>
 </x-front::layout.sidebar>
 
--- Modules/Frontend/resources/views/post.blade.php
+++ Modules/Frontend/resources/views/post.blade.php
@@ -8,20 +8,16 @@ if (!$page) {
     echo view('front::not-found');
     return;
 }
-SEO::title($page->title)
-    ->description($page->excerpt ?? '')
-    ->keywords($page->keywords ?? '');
-SEO::openGraphType('WebPage');
-SEO::openGraphSiteName(settings('site_title', ''));
-SEO::openGraphTitle($page->title);
-SEO::openGraphUrl(url($page->slug));
-if ($page->image) {
-    SEO::openGraphImage(url($page->image));
-}
 
 $content = \Blade::render($page->content);
 ?>
 <x-front::layout.sidebar>
+    @seoTitle($page->title)
+    @seoDescription($page->excerpt ?? '')
+    @seoKeywords($page->excerpt ?? '')
+    @if ($page->image)
+        @seoOpenGraphImage(url($page->image));
+    @endif
     <h1 class="text-center mb-5 text-neutral-900 text-left font-semibold text-3xl md:text-4xl md:!leading-[120%] lg:text-5xl dark:text-neutral-100 max-w-4xl "
         title="{{$page->title}}">{{$page->title}}</h1>
     <span class="block text-base text-neutral-500 md:text-lg dark:text-neutral-400 pb-1">{{$page->excerpt}}</span>
