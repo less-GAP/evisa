409ed5bf7a07b8c81269c250cb8807b86107cb92
Phuong <phuong@lessgap.com>
2023-09-22 10:40:31 +0700

    hotfix homepage

--- Modules/Frontend/resources/views/post.blade.php
+++ Modules/Frontend/resources/views/post.blade.php
@@ -15,9 +15,7 @@ $content = \Blade::render($page->content);
     @seoTitle($page->title)
     @seoDescription($page->excerpt ?? '')
     @seoKeywords($page->excerpt ?? '')
-    @if ($page->image)
-        @seoOpenGraphImage(url($page->image));
-    @endif
+
     <h1 class="text-center mb-5 text-neutral-900 text-left font-semibold text-3xl md:text-4xl md:!leading-[120%] lg:text-5xl dark:text-neutral-100 max-w-4xl "
         title="{{$page->title}}">{{$page->title}}</h1>
     <span class="block text-base text-neutral-500 md:text-lg dark:text-neutral-400 pb-1">{{$page->excerpt}}</span>
