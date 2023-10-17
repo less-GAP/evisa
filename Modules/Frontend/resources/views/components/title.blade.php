d2956c5cadce3061b3fa70a9a2abe47996466698
Phuong <phuong@lessgap.com>
2023-10-17 20:23:59 +0700

    add contact

--- Modules/Frontend/resources/views/components/title.blade.php
+++ Modules/Frontend/resources/views/components/title.blade.php
@@ -1,19 +1,7 @@
-<div class="title-wrapper relative my-10 min-h-[40px] xl:min-h-[75px] overflow-hidden">
-    <div class="absolute top-0 left-0 z-10 flex flex-col justify-end w-full h-full">
-        <div class="bg-white h-1/3"></div>
-        <div class="h-1/3"
-             style="background: linear-gradient(to right, rgb(233, 68, 220) 0%, rgb(35, 171, 169) 25%, rgb(230, 214, 36) 50%, rgb(35, 171, 169) 75%, rgb(233, 68, 220) 100%) center center / 200%;"></div>
-        <div class="bg-white h-1/3"></div>
-    </div>
-    <div class="absolute top-0 z-20 flex flex-col w-auto h-full -translate-x-1/2 left-1/2">
-        <div class="relative flex flex-col items-center justify-center h-full m-0 whitespace-no-wrap">
-            <div class="hidden h-1/3"></div>
-            <div class="absolute bg-white h-1/3 mid-skew"></div>
-            <div class="hidden h-1/3"></div>
-            <div><h2
-                    class="title font-black mb-0 mt-2 relative text-center uppercase text-xl md:text-[42px] lg:text-[56px] 2xl:text-[72px] whitespace-nowrap">
-                    {{$slot}}
-                </h2></div>
-        </div>
-    </div>
+<div class="pb-10 pt-5 px-0">
+
+    <h2
+        class="title font-black  mb-5 mt-5 relative text-left uppercase text-xl md:text-[42px] lg:text-[56px] 2xl:text-[72px] whitespace-nowrap">
+        {{$slot}}
+    </h2>
 </div>
--- Modules/Frontend/resources/views/login.blade.php
+++ Modules/Frontend/resources/views/login.blade.php
@@ -1,26 +1,6 @@
 <x-front::layout.default>
     <div class="container mx-auto">
-        <div class="title-wrapper relative min-h-[40px] xl:min-h-[75px] overflow-hidden">
-            <div class="absolute top-0 left-0 z-10 flex flex-col justify-end w-full h-full">
-                <div class="bg-white h-1/3"></div>
-                <div class="h-1/3"
-                     style="background: linear-gradient( to right, rgb(233,68,220) 0%, rgb(35,171,169) 25%, rgb(230,214,36) 50%, rgb(35,171,169) 75%, rgb(233,68,220) 100%); background-size: 200%; background-position: center;">
-                </div>
-                <div class="bg-white h-1/3"></div>
-            </div>
-            <div class="absolute top-0 z-20 flex flex-col w-auto h-full -translate-x-1/2 left-1/2">
-                <div class="relative flex flex-col items-center justify-center h-full m-0 whitespace-no-wrap">
-                    <div class="hidden h-1/3"></div>
-                    <div class="absolute bg-white h-1/3 mid-skew"></div>
-                    <div class="hidden h-1/3"></div>
-                    <div>
-                        <h1
-                            class="title font-black mb-0 mt-2 relative text-center uppercase text-xl md:text-[42px] lg:text-[56px] 2xl:text-[72px] whitespace-nowrap">
-                            SIGNIN</h1>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <x-front::title>Login</x-front::title>
         <div class="flex  justify-center px-6 my-12">
             <!-- Row -->
             <x-splade-form :validateTrigger="'submit'" action="/login" method="POST">
@@ -45,12 +25,12 @@
                             :spinner="true"/>
                     @endif
 
-                    <div class="inline-flex w-full">
-                        <Link class="mr-5 w-1/2" href="/reset-password">
-                            <x-front::button link>Reset</x-front::button>
+                    <div class="inline-flex mt-3 w-full">
+                        <Link class="mr-5" href="/reset-password">
+                            Reset password
                         </Link>
-                        <Link class=" w-1/2" href="/signup">
-                            <x-front::button link>Signup</x-front::button>
+                        <Link  href="/signup">
+                            Signup
                         </Link>
                     </div>
                     @if(settings('login_google')=='active')
