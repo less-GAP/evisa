b258fca753fd1f4da2c93dd7de1f7647638b219a
Nguyen Pham <admin@madelab.dev>
2023-11-09 10:31:29 -0600

    toolbar

--- Modules/Admin/src/components/HtmlEditor.vue
+++ Modules/Admin/src/components/HtmlEditor.vue
@@ -4,7 +4,12 @@ v-model="modelValue" :api-key="$appState.editorKey" model-events="change keydown
     menubar: true,
     branding: false,
     plugins: 'autolink fullscreen charmap  emoticons  link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect  typography inlinecss',
-    toolbar: 'add_image | fullscreen | undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link  media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+    // toolbar: 'add_image | fullscreen | undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link  media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+    // 2 line toolbar
+    toolbar: [
+      'add_image | undo redo | blocks fontfamily fontsize fullscreen', 
+      'bold italic underline strikethrough link  media table mergetags, align lineheight tinycomments checklist numlist bullist indent outdent emoticons charmap removeformat',
+    ],
     tinycomments_mode: 'inline',
     tinycomments_author: 'Author name',
     apply_source_formatting : true,
